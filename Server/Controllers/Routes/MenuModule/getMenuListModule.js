82807a5fecfbdf9adb5ab280a0f3185fa30ee724
Akshay Jain <coolakshayjain557@gmail.com>
2021-06-19 11:24:33 +0530

    config cluster and add get menu api base on role

--- Server/Controllers/Routes/MenuModule/getMenuListModule.js
+++ Server/Controllers/Routes/MenuModule/getMenuListModule.js
@@ -3,13 +3,22 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi)
 
 const errorResponseHelper = require('../../../Helper/errorResponse');
-
+const menuModuleSchema = Joi.object({
+    userRole: Joi.array().required()
+});
 
 function getMenuList(Models)
 {
     async function menuList(req, res) {
         try {
-            let findData = await Models.MenuModuleDB.find({});            
+            let validateData = menuModuleSchema.validate(req.body);
+            if (validateData.error) {
+                throw { status: false, error: validateData, message: CONSTANTSMESSAGE.INVALID_DATA };
+            }
+
+            // pick data from req.body
+            let bodyData = _.pick(req.body, ['userRole']);
+            let findData = await Models.MenuModuleDB.find({'name': {$in:bodyData.userRole}});            
             res.send({ status: true, message: "", data:findData });
         }
         catch (e) {
