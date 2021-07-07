53365ae5561ffc3889e757d7a32ebba9f4c15cbc
Akshay Jain <coolakshayjain557@gmail.com>
2021-07-07 19:46:43 +0530

    signup api bug fix

--- Server/Controllers/Routes/index.js
+++ Server/Controllers/Routes/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     forgotPasswordFunction: require('./Authentication/forgotPassword'),
-    signupFunction: require('./Authentication/signup'),
     loginFunction: require('./Authentication/login'),
     verificationFunction: require('./Authentication/verification'),
     setNewPasswordFunction: require('./Authentication/setNewPassword'),
