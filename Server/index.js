4ec61fef3cd82e82ee3131f14aa97108947d0a4a
Vikram <vikramsinwal@gmail.com>
2021-07-11 18:09:45 +0530

    cors update

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ app.use(helmet({
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     credentials: true
 // }));
-app.use(cors());
+app.use(cors({ origin: '*' }))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
