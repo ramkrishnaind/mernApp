cc49a3ae8ea1f2084796cceb265d8f1800de6d90
Vijay Kumar <vijkumar@mafcarrefour.com>
2021-07-18 14:54:24 +0530

    Completed Client App Property-List and Property-Detail Page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {Container} from '@material-ui/core';
 import routes from '../src/routes';
 import {useSelector} from 'react-redux';
+import Header from '../src/components/header';
+import Footer from '../src/components/footer';
 
 const App = () => {
   const isAuth = useSelector(state => state.Login.isAuth);
 
   return (
     <Router>
+      <Header />
       <Switch>
         {routes.map((route, i) => (
           <Route exact={route.exact} path={route.path} render={() => route.render({isAuth})} key={i} />
         ))}
       </Switch>
+      <div className="footer-section">
+          <Container style={{paddingTop: 10, paddingBottom: 40}}>
+            <Footer />
+          </Container>
+        </div>
     </Router>
   );
 }
