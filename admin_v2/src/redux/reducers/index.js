fa96170c97ee0c05b1a16d236732e2a911d5f0fe
Vikram <vikramsinwal@gmail.com>
2021-07-22 22:47:34 +0530

    conflict resolve

--- admin_v2/src/redux/reducers/index.js
+++ admin_v2/src/redux/reducers/index.js
@@ -5,13 +5,10 @@ import SideMenuListReducer from './SideMenuListReducer';
 import menu from './MenuReducer';
 import ModuleReducer from './ModuleRightsReducer';
 import user from './UserReducer';
-<<<<<<< HEAD
 import enquiry from'./EnquiryReducer';
 import callback from'./CallbackReducer';
 
-=======
 import UserRoleReducer from './createUserRoleReducer'
->>>>>>> de593ea9fec87d1b60b225509b79514f35e4f98e
 const reducers =  {
     Login: LoginReducer,
     Register: RegisterReducer,
@@ -20,12 +17,9 @@ const reducers =  {
     menu: menu,
     ModuleReducer : ModuleReducer,
     user : user,
-<<<<<<< HEAD
     enquiry:enquiry,
     callback:callback,
-=======
     UserRoleReducer : UserRoleReducer
->>>>>>> de593ea9fec87d1b60b225509b79514f35e4f98e
 }
 
 export default reducers;
