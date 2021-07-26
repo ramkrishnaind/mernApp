928cd2201bdb0c4e12e9062c06e5c6637335cd94
samip <kothiyasamip@gmail.com>
2021-07-26 22:02:22 +0530

    blog section add

--- admin_v2/src/redux/reducers/index.js
+++ admin_v2/src/redux/reducers/index.js
@@ -5,25 +5,26 @@ import SideMenuListReducer from './SideMenuListReducer';
 import menu from './MenuReducer';
 import ModuleReducer from './ModuleRightsReducer';
 import user from './UserReducer';
-import enquiry from'./EnquiryReducer';
-import callback from'./CallbackReducer';
-import snackbar from'./snackbarReducer';
-import review from'./reviewReducer';
-import career from'./CareerReducer';
-
-const reducers =  {
+import enquiry from './EnquiryReducer';
+import callback from './CallbackReducer';
+import snackbar from './snackbarReducer';
+import review from './reviewReducer';
+import career from './CareerReducer';
+import blog from './blogReducer';
+const reducers = {
     Login: LoginReducer,
     Register: RegisterReducer,
     PropertyList: PropertyListReducer,
     SideMenuList: SideMenuListReducer,
     menu: menu,
-    ModuleReducer : ModuleReducer,
-    user : user,
-    enquiry:enquiry,
-    callback:callback,
-    snackbar:snackbar,
-    review:review,
-    career:career,
+    ModuleReducer: ModuleReducer,
+    user: user,
+    enquiry: enquiry,
+    callback: callback,
+    snackbar: snackbar,
+    review: review,
+    career: career,
+    blog: blog,
 }
 
 export default reducers;
