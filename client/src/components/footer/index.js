66ece44cc92aca943655b70c8e69b9b9788fbf0a
vishal123-oss <curiousvishal.sharma@gmail.com>
2021-09-13 01:18:49 +0530

    add dynamic content to blog page and created right side of blog page

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Typography, Grid, Container, makeStyles, Button, Box, TextField, IconButton } from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import {Typography, Grid, Container, makeStyles, Button, Box, TextField, IconButton} from "@material-ui/core";
 import './footer.css';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import SendIcon from '@material-ui/icons/Send';
@@ -8,7 +8,11 @@ import PinterestIcon from '@material-ui/icons/Pinterest';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import LanguageIcon from '@material-ui/icons/Language';
 import Mobilefootermenu from '../footer/footermobile';
+import ApiClient from "../../api-client";
 import './footer.css';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
 
 const useStyles = makeStyles((theme) => ({
     text1: {
@@ -63,36 +67,65 @@ const useStyles = makeStyles((theme) => ({
 
 const Footer = props => {
     const classes = useStyles();
+    const [footerDetails, setFooterDetails] = useState({});
+    const [socialLinks, setSocialLinks] = useState({});
+    useEffect(() => {
+        populateFooterDetails();
+        populateSocialMediaLinks();
+    }, []);
+
+    const populateSocialMediaLinks = () => {
+
+        const getData = async () => {
+            const response = await ApiClient.call(ApiClient.REQUEST_METHOD.POST, '/home/getFooterSocialMedia', {}, {}, {Cookie: ApiClient.cookie, Authorization: ApiClient.authorization}, false);
+            setSocialLinks(response.data);
+            localStorage.setItem('social-links', JSON.stringify(response.data));
+            console.log('populateSocialMediaLinks details', response.data);
+        };
+        getData();
+
+    };
+    const populateFooterDetails = () => {
+
+        const getData = async () => {
+            const response = await ApiClient.call(ApiClient.REQUEST_METHOD.POST, '/home/getFooterAddress', {}, {}, {Cookie: ApiClient.cookie, Authorization: ApiClient.authorization}, false);
+            setFooterDetails(response.data);
+            localStorage.setItem('company_detials', JSON.stringify(response.data));
+            // console.log('populateFooterDetails details', response);
+        };
+        getData();
+
+    };
 
     return (
         <>
 
             <Grid container spacing={3}>
 
-                <Grid item xs={12} md={4} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
+                <Grid item xs={12} md={4} style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
                     <Typography className={classes.text1}>VISHAL CONSTRUCTION</Typography>
                     <Grid contaienr>
                         <Grid item xs={12} md={12} className={classes.footer_column1}>
-                            <LocationOnIcon style={{ color: '#06AEB8', fontSize: 20, padding: 0, marginRight: 8 }} />
-                            <Typography className={classes.text3}>9-A, Brij Vatika, 7 No. Bus Stand, Mahal Road, Jagatpura, Jaipur - 302017 (Rajasthan)</Typography>
+                            <LocationOnIcon style={{color: '#06AEB8', fontSize: 20, padding: 0, marginRight: 8}} />
+                            <Typography className={classes.text3}>{footerDetails?.adress} {footerDetails?.city} {footerDetails?.state} - {footerDetails?.pinCode}</Typography>
                         </Grid>
                         <Grid item xs={12} md={12} className={classes.footer_column1}>
-                            <LocationOnIcon style={{ color: '#06AEB8', fontSize: 20, padding: 0, marginRight: 8 }} />
-                            <Typography className={classes.text3}>+91-9571647680</Typography>
+                            <PhoneIphoneIcon style={{color: "#06AEB8", fontSize: 20, padding: 0, marginRight: 8, }} />
+                            <Typography className={classes.text3}>{footerDetails?.mobile}</Typography>
                         </Grid>
                         <Grid item xs={12} md={12} className={classes.footer_column1}>
-                            <LocationOnIcon style={{ color: '#06AEB8', fontSize: 20, padding: 0, marginRight: 8 }} />
-                            <Typography className={classes.text3}>info@vishalconstructioncompany.com</Typography>
+                            <MailOutlineIcon style={{color: "#06AEB8", fontSize: 20, padding: 0, marginRight: 8, }} />
+                            <Typography className={classes.text3}>{footerDetails?.email}</Typography>
                         </Grid>
                         <Grid item xs={12} md={12} className={classes.footer_column1}>
-                            <LocationOnIcon style={{ color: '#06AEB8', fontSize: 20, padding: 0, marginRight: 8 }} />
-                            <Typography className={classes.text3}>Mon - Sat, 08 AM - 06 PM</Typography>
+                            <AccessTimeIcon style={{color: "#06AEB8", fontSize: 20, padding: 0, marginRight: 8, }} />
+                            <Typography className={classes.text3}>{footerDetails?.timming}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={12} md={4} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
+                <Grid item xs={12} md={4} style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
                     <Typography className={classes.text1}>POPULAR LINKS</Typography>
-                    <Grid contaienr style={{ width: '100%', display: 'flex', marginTop: 10 }}>
+                    <Grid contaienr style={{width: '100%', display: 'flex', marginTop: 10}}>
                         <Grid item xs={6} md={6}>
                             <Box>
                                 <Typography className={`${classes.text3} links`}>Home</Typography>
@@ -112,40 +145,50 @@ const Footer = props => {
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={12} md={4} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
+                <Grid item xs={12} md={4} style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
                     <Typography className={classes.text1}>NEWSLETTER</Typography>
                     <Typography className={classes.text3}>Subscribe your email to get the latest news and new offer also discount</Typography>
                     <Grid container>
-                        <Grid item style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10 }}>
-                            <TextField placeholder="Email Address" style={{ backgroundColor: '#FFFFFF', border: 'none' }} />
-                            <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 5, backgroundColor: '#FF7601', marginLeft: 8 }}>
-                                <SendIcon style={{ color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                        <Grid item style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10}}>
+                            <TextField placeholder="Email Address" style={{backgroundColor: '#FFFFFF', border: 'none'}} />
+                            <Box style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 5, backgroundColor: '#FF7601', marginLeft: 8}}>
+                                <SendIcon style={{color: '#FFFFFF', fontSize: 20, padding: 0, }} />
                             </Box>
                         </Grid>
                     </Grid>
                     <Typography className={classes.text2}>Follow us on</Typography>
                     <Grid container>
-                        <Grid item style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10 }}>
+                        <Grid item style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10}}>
                             <Box className={`${classes.socialBox} social_icon`}>
-                                <SendIcon style={{ color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                <a href={socialLinks?.send}>
+                                    <SendIcon style={{color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                </a>
                             </Box>
                             <Box className={`${classes.socialBox} social_icon`}>
-                                <TwitterIcon style={{ color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                <a href={socialLinks?.twitter}>
+                                    <TwitterIcon style={{color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                </a>
                             </Box>
                             <Box className={`${classes.socialBox} social_icon`}>
-                                <PinterestIcon style={{ color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                <a href={socialLinks?.pinterest}>
+                                    <PinterestIcon style={{color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                </a>
                             </Box>
                             <Box className={`${classes.socialBox} social_icon`}>
-                                <YouTubeIcon style={{ color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                <a href={socialLinks?.youtube}>
+                                    <YouTubeIcon style={{color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                </a>
                             </Box>
                             <Box className={`${classes.socialBox} social_icon`}>
-                                <LanguageIcon style={{ color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                <a href={socialLinks?.language}>
+                                    <LanguageIcon style={{color: '#FFFFFF', fontSize: 20, padding: 0, }} />
+                                </a >
                             </Box>
                         </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid container style={{ marginTop: 40 }} spacing={2}>
+            <Grid container style={{marginTop: 40}} spacing={2}>
                 <Grid xs={12} md={2}>
                     <Typography className={classes.text4}>1BHK Flats in Jaipur</Typography>
                 </Grid>
@@ -165,18 +208,18 @@ const Footer = props => {
                     <Typography className={classes.text4}>Villas in Jaipur</Typography>
                 </Grid>
             </Grid>
-            <Grid container style={{ marginTop: 40 }} spacing={2}>
-                <Grid xs={12} md={12} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Grid container style={{marginTop: 40}} spacing={2}>
+                <Grid xs={12} md={12} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                     <Typography className={classes.text3}>© 2021 Design by Dzone India Software & Technologies Pvt. Ltd All Right Reserved</Typography>
                 </Grid>
 
             </Grid>
 
-            <Grid container style={{ marginTop: 40 }} spacing={2} className="footerSplit">
+            <Grid container style={{marginTop: 40}} spacing={2} className="footerSplit">
                 <Mobilefootermenu />
             </Grid>
         </>
     );
-}
+};
 
-export default Footer;
+export default Footer;;
--- client/src/pages/carrer/index.js
+++ client/src/pages/carrer/index.js
@@ -1,11 +1,11 @@
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, {useEffect, useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import { Box, Button, Container } from '@material-ui/core';
+import {Box, Button, Container} from '@material-ui/core';
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import DateRangeIcon from '@material-ui/icons/DateRange';
-import "./index.css"
+import "./index.css";
 import PageBanner from "../../components/page-banner";
 import bannerImage from "../../images/clientbg.jpeg";
 import Workspace1 from "../../images/carrer_img/about_carrier.png";
@@ -15,143 +15,108 @@ import Workspace4 from "../../images/carrer_img/about_carrier4.png";
 import Workspace5 from "../../images/carrer_img/about_carrier6.png";
 import Workspace6 from "../../images/carrer_img/about_carrier7.png";
 import ApplyJobs from '../../components/apply-jobs/apply-jobs';
+import ApiClient from '../../api-client';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
 }));
 
 export default function Carrer() {
-  const classes = useStyles();
+    const classes = useStyles();
+    const [allActiveCareer, setAllActiveCareer] = useState([]);
 
-  return (
-      <>
-      <PageBanner
-        bgImage={bannerImage}
-        title="Career"
-        currentPage="CAREER"
-      />
-      <Box className="CareerPageText">
-        <Container>
-            <div className={classes.root} className="headingtext">                
-                <Box className="middel-content"> 
-                    {/* <CarrierBannerImg /> */}
-                    <h2>Come, Join Us! <span style={{color:"#00afb8"}}>We’re Hiring.</span></h2>
+    useEffect(() => {
+        populateBlogList();
+    }, []);
 
-                    <p>We believe that each one of us should be able to find our dream job, and we constantly strive hard to make that possible. Apply now!</p>
+    const populateBlogList = () => {
+        const getData = async () => {
+            const response = await ApiClient.call(ApiClient.REQUEST_METHOD.POST, '/career/getAllActiveCareer', {}, {}, {Cookie: ApiClient.cookie, Authorization: ApiClient.authorization}, false);
+            setAllActiveCareer(response.data);
+            console.log('setAllActiveCareer details', response.data);
+        };
+        getData();
+    };
 
-                    <div className="job-list">
-                        <div className="row">
-                            <div className=" job_inner wow fadeInUp" >
-                            <Grid item xs={9}>
-                                    <h3 className="job-title">Assistant Manager - Marketing - 1 Position</h3>
-                                    <div className="job-details-section">
-                                        <div className="job-detail">                                        
-                                            <span className="detail-value"> <DateRangeIcon style={{ color: "#FF7601",  }} />7-11 yrs</span>
-                                        </div>
-                                        <div className="job-detail">
-                                            <span className="detail-value"><LocationOnIcon style={{ color: "#FF7601",  }} /> Jaipur</span>
-                                        </div>
-                                    </div>
-                            </Grid>
-                                <Grid item xs={3}>
-                                    <Box className="ParentButton">
-                                        <ApplyJobs buttonName={"Apply Now"} Jobsheading={""} />
-                                    </Box>
-                                </Grid>
+    return (
+        <>
+            <PageBanner
+                bgImage={bannerImage}
+                title="Career"
+                currentPage="CAREER"
+            />
+            <Box className="CareerPageText">
+                <Container>
+                    <div className={classes.root} className="headingtext">
+                        <Box className="middel-content">
+                            {/* <CarrierBannerImg /> */}
+                            <h2>Come, Join Us! <span style={{color: "#00afb8"}}>We’re Hiring.</span></h2>
+
+                            <p>We believe that each one of us should be able to find our dream job, and we constantly strive hard to make that possible. Apply now!</p>
+
+                            <div className="job-list" >
+                                <div className="row">
+
+                                    {(allActiveCareer || []).map((career, i) => {
+                                        const {_id, degination, department, vacancy, experiance, location} = career;
+
+                                        return (<div className=" job_inner wow fadeInUp" >
+                                            <Grid item xs={9}>
+                                                <h3 className="job-title">{degination} - {department} - {vacancy || 0} Position</h3>
+                                                <div className="job-details-section">
+                                                    <div className="job-detail">
+                                                        <span className="detail-value"> <DateRangeIcon style={{color: "#FF7601", }} />{experiance}</span>
+                                                    </div>
+                                                    <div className="job-detail">
+                                                        <span className="detail-value"><LocationOnIcon style={{color: "#FF7601", }} /> {location}</span>
+                                                    </div>
+                                                </div>
+                                            </Grid>
+                                            <Grid item xs={3}>
+                                                <Box className="ParentButton">
+                                                    <ApplyJobs buttonName={"Apply Now"} Jobsheading={""} />
+                                                </Box>
+                                            </Grid>
+                                        </div>);
+                                    })}
+                                </div>
                             </div>
-                            <div className=" job_inner wow fadeInUp" >
-                            <Grid item xs={9}>
-                                    <h3 className="job-title">Assistant Manager - Marketing - 1 Position</h3>
-                                    <div className="job-details-section">
-                                        <div className="job-detail">                                        
-                                            <span className="detail-value"> <DateRangeIcon style={{ color: "#FF7601",  }} />7-11 yrs</span>
-                                        </div>
-                                        <div className="job-detail">
-                                            <span className="detail-value"><LocationOnIcon style={{ color: "#FF7601",  }} /> Jaipur</span>
-                                        </div>
-                                    </div>
-                            </Grid>
-                                <Grid item xs={3}>
-                                    <Box className="ParentButton">
-                                        <ApplyJobs buttonName={"Apply Now"} Jobsheading={""} />
-                                    </Box>
+                        </Box>
+                        <Box className="work-space">
+                            <h2>Vishal <span style={{color: "#00afb8"}}>Work Space</span></h2>
+                            <Grid container spacing={3}>
+                                <Grid item xs={4} className="workSpaceImgaes">
+                                    <img src={process.env.PUBLIC_URL + Workspace1} />
                                 </Grid>
-                            </div>
-                            <div className=" job_inner wow fadeInUp" >
-                            <Grid item xs={9}>
-                                    <h3 className="job-title">Assistant Manager - Marketing - 1 Position</h3>
-                                    <div className="job-details-section">
-                                        <div className="job-detail">                                        
-                                            <span className="detail-value"> <DateRangeIcon style={{ color: "#FF7601",  }} />7-11 yrs</span>
-                                        </div>
-                                        <div className="job-detail">
-                                            <span className="detail-value"><LocationOnIcon style={{ color: "#FF7601",  }} /> Jaipur</span>
-                                        </div>
-                                    </div>
-                            </Grid>
-                                <Grid item xs={3}>
-                                    <Box className="ParentButton">
-                                        <ApplyJobs buttonName={"Apply Now"} Jobsheading={""} />
-                                    </Box>
+                                <Grid item xs={4} className="workSpaceImgaes">
+                                    <img src={process.env.PUBLIC_URL + Workspace2} />
                                 </Grid>
-                            </div>
-                            <div className=" job_inner wow fadeInUp" >
-                            <Grid item xs={9}>
-                                    <h3 className="job-title">Assistant Manager - Marketing - 1 Position</h3>
-                                    <div className="job-details-section">
-                                        <div className="job-detail">                                        
-                                            <span className="detail-value"> <DateRangeIcon style={{ color: "#FF7601",  }} />7-11 yrs</span>
-                                        </div>
-                                        <div className="job-detail">
-                                            <span className="detail-value"><LocationOnIcon style={{ color: "#FF7601",  }} /> Jaipur</span>
-                                        </div>
-                                    </div>
-                            </Grid>
-                                <Grid item xs={3}>
-                                    <Box className="ParentButton">
-                                        <ApplyJobs buttonName={"Apply Now"} Jobsheading={""} />
-                                        {/* <Button>Apply Now</Button> */}
-                                    </Box>
+                                <Grid item xs={4} className="workSpaceImgaes">
+                                    <img src={process.env.PUBLIC_URL + Workspace3} />
                                 </Grid>
-                            </div>
-                        </div>
+                            </Grid>
+                            <Grid container spacing={3}>
+                                <Grid item xs={4} className="workSpaceImgaes">
+                                    <img src={process.env.PUBLIC_URL + Workspace4} />
+                                </Grid>
+                                <Grid item xs={4} className="workSpaceImgaes">
+                                    <img src={process.env.PUBLIC_URL + Workspace5} />
+                                </Grid>
+                                <Grid item xs={4} className="workSpaceImgaes">
+                                    <img src={process.env.PUBLIC_URL + Workspace6} />
+                                </Grid>
+                            </Grid>
+                        </Box>
                     </div>
-                </Box>
-                <Box className="work-space">
-                    <h2>Vishal <span style={{color:"#00afb8"}}>Work Space</span></h2>
-                    <Grid container spacing={3}>
-                        <Grid item xs={4} className="workSpaceImgaes">
-                            <img src={process.env.PUBLIC_URL + Workspace1} />
-                        </Grid>
-                        <Grid item xs={4} className="workSpaceImgaes">
-                            <img src={process.env.PUBLIC_URL + Workspace2} />
-                        </Grid>
-                        <Grid item xs={4} className="workSpaceImgaes">
-                            <img src={process.env.PUBLIC_URL + Workspace3} />
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={3}>
-                        <Grid item xs={4} className="workSpaceImgaes">
-                            <img src={process.env.PUBLIC_URL + Workspace4} />
-                        </Grid>
-                        <Grid item xs={4} className="workSpaceImgaes">
-                            <img src={process.env.PUBLIC_URL + Workspace5} />
-                        </Grid>
-                        <Grid item xs={4} className="workSpaceImgaes">
-                            <img src={process.env.PUBLIC_URL + Workspace6} />
-                        </Grid>
-                    </Grid>
-                </Box>
-            </div>
-        </Container>
-    </Box>
-    </>
-  );
+                </Container>
+            </Box>
+        </>
+    );
 }
