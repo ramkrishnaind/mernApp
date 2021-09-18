2377df27cc9005d49017a8c35dd28f4b54c91c6e
Arjun Singh <arjun3a@users.noreply.github.com>
2021-09-18 19:58:47 +0530

    Homepage and Property Detail Page Design
    Homepage and Property Detail Page Design

--- client/src/components/outer-carousel-slider/index.js
+++ client/src/components/outer-carousel-slider/index.js
@@ -1,50 +1,70 @@
-import React, {useState} from "react";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./outer-carousel-slider.css";
 import PropertyViewCard from "../property-view-card";
-import {Typography, Grid, Container, makeStyles, Button, Box} from "@material-ui/core";
+import { Typography, Grid, Container, makeStyles, Button, Box } from "@material-ui/core";
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import LocalHotelIcon from '@material-ui/icons/LocalHotel';
-import {Link as RouterLink} from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
 import BathtubIcon from '@material-ui/icons/Bathtub';
 import DriveEtaIcon from '@material-ui/icons/DriveEta';
 // import './property-view-card.css';
 import InnerCarouselSlider from "../inner-carousel-slider";
-import {CustomNoRowsOverlay} from '../../components/no-data-found/no-data-found';
+import { CustomNoRowsOverlay } from '../../components/no-data-found/no-data-found';
 import './featured.css';
 
 const useStyles = makeStyles((theme) => ({
-    
+
 }));
 
-const options = {
-    margin: 24,
-    responsiveClass: true,
-    nav: true,
+
+
+const settings = {
     dots: false,
-    autoplay: true,
-    navText: ["Prev", "Next"],
-    smartSpeed: 1000,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 10,
-        },
-        576: {
-            items: 2,
-            margin: 10,
-        },
-        768: {
-            items: 2,
-            margin: 15,
-        },
-        992: {
-            items: 3,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    initialSlide: 0,
+    draggable: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,         
         }
-    },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,          
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,         
+        }
+      }
+    ]
+  };
+
+const settings1 = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',   
 };
 
 const OuterCarouselSlider = (props) => {
@@ -61,68 +81,79 @@ const OuterCarouselSlider = (props) => {
     return (
         <div>
             {total > 0 ?
-                <OwlCarousel items={Math.min(3, total)} className="owl-theme" {...options}>
+                <Slider items={Math.min(3, total)} className="property-carousel" {...settings}>
                     {data.map((item, i) => {
-                        const {_id, userId, propertyDetails, status, iAm, pType, postingAs, nameOfProject, propertTag, created, updated, __v, features} = item;
+                        const { _id, userId, propertyDetails, status, iAm, pType, postingAs, nameOfProject, propertTag, created, updated, __v, features } = item;
                         const propertyFor = item.for;
                         return (
                             <Box key={i} className="property-item">
                                 <Grid contaienr className="property-wrap">
-                                   
-                                        {/* <InnerCarouselSlider /> */}
-                                        <Grid className="property-image" style={{position: 'relative'}}>
-                                            {propertTag ? <span class="featured">{propertTag}</span> : null}
-                                            <img className="img" src={process.env.PUBLIC_URL + '/property_img3.jpeg'} />
-                                        </Grid>
 
-                                        <Grid className="property-summery">                                         
-                                            <Box component="span" className="property-tag">{pType}</Box>
-                                            <Typography variant="h3" className="property-title">{nameOfProject}</Typography>
-                                            
-                                            <Grid container className="property-information">
-                                                <Grid item xs={6} md={6} className="property-feature">
-                                                    <ZoomOutMapIcon />
-                                                    <Typography>{features[0]?.builtUpArea} Sq-Ft</Typography>
-                                                </Grid>
-                                                <Grid item xs={6} md={6} className="property-feature">
-                                                    <LocalHotelIcon  />
-                                                    <Typography >{features[0]?.bedrooms} Bedrooms</Typography>
-                                                </Grid>
-                                                <Grid item xs={6} md={6} className="property-feature">
-                                                    <LocalHotelIcon />
-                                                    <Typography>{features[0]?.balconies} Balconies</Typography>
-                                                </Grid>
-                                                <Grid item xs={6} md={6} className="property-feature">
-                                                    <BathtubIcon />
-                                                    <Typography>{features[0]?.bathrooms} Bathroom</Typography>
-                                                </Grid>
+                                    {/* <InnerCarouselSlider /> */}
+                                    <Grid className="property-image" style={{ position: 'relative' }}>
+                                        {propertTag ? <span class="featured">{propertTag}</span> : null}
+                                        {/* <img className="img" src={process.env.PUBLIC_URL + '/property_img3.jpeg'} /> */}
+                                        <Slider {...settings1}>
+                                            <Box className="property-image-thumb">
+                                                <img src={process.env.PUBLIC_URL + '/property_img3.jpeg'} />                                                
+                                            </Box>
+                                            <Box className="property-image-thumb">
+                                                <img src={process.env.PUBLIC_URL + '/property_img3.jpeg'} />                                                
+                                            </Box>
+                                            <Box className="property-image-thumb">
+                                                <img src={process.env.PUBLIC_URL + '/property_img3.jpeg'} />                                               
+                                            </Box>                                            
+                                        </Slider>
+                                    </Grid>
+
+                                    <Grid className="property-summery">
+                                        <Box component="span" className="property-tag">{pType}</Box>
+                                        <Typography variant="h3" className="property-title">{nameOfProject}</Typography>
+
+                                        <Grid container className="property-information">
+                                            <Grid item xs={6} md={6} className="property-feature">
+                                                <ZoomOutMapIcon />
+                                                <Typography>{features[0]?.builtUpArea} Sq-Ft</Typography>
                                             </Grid>
-                                            <Grid container className="property-button">                                              
-                                                <Box className="btn btn-primary" component={RouterLink} to={
-                                                    {
-                                                        pathname: '/home-detail',
-                                                        state: _id
-                                                    }}>
-                                                    MORE DETAIL
-                                                </Box>                                                
-                                                <Box className="btn btn-secondary">
-                                                    CALL NOW
-                                                </Box>                                                
+                                            <Grid item xs={6} md={6} className="property-feature">
+                                                <LocalHotelIcon />
+                                                <Typography >{features[0]?.bedrooms} Bedrooms</Typography>
                                             </Grid>
-                                        </Grid>    
-                                       
-                                        {/* <Grid contaienr>
+                                            <Grid item xs={6} md={6} className="property-feature">
+                                                <LocalHotelIcon />
+                                                <Typography>{features[0]?.balconies} Balconies</Typography>
+                                            </Grid>
+                                            <Grid item xs={6} md={6} className="property-feature">
+                                                <BathtubIcon />
+                                                <Typography>{features[0]?.bathrooms} Bathroom</Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid container className="property-button">
+                                            <Box className="btn btn-primary" component={RouterLink} to={
+                                                {
+                                                    pathname: '/home-detail',
+                                                    state: _id
+                                                }}>
+                                                MORE DETAIL
+                                            </Box>
+                                            <Box className="btn btn-secondary">
+                                                CALL NOW
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+
+                                    {/* <Grid contaienr>
                                         <Grid item style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10}}>
                                             <LocationOnIcon style={{color: '#FF7601', fontSize: 20, padding: 0, marginRight: 8}} />
                                             <Typography className={classes.text3}>{iAm}</Typography>
                                         </Grid>
                                     </Grid> */}
-                                 
+
                                 </Grid>
                             </Box>
                         );
                     })}
-                </OwlCarousel> : <CustomNoRowsOverlay />}
+                </Slider> : <CustomNoRowsOverlay />}
         </div>
     );
 };
