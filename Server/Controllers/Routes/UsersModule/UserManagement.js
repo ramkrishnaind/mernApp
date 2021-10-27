4981ae5566963c82d53c09a33d02290caf3768f0
vikramsinwal <vikramsinwal@gmail.com>
2021-10-28 00:43:07 +0530

    Booking api Update

--- Server/Controllers/Routes/UsersModule/UserManagement.js
+++ Server/Controllers/Routes/UsersModule/UserManagement.js
@@ -262,16 +262,19 @@ function getUserBookings(Models) {
 
             // Getting User from Database
             let Data = await Models.BookingDB.find({ userId: req.body.userId }).populate('propertyId');
+            let result = [];
             for (let x = 0; x < Data.length; x++) {
-                let propertyId = Data[x].propertyId._id;
+                let item = Data[x].toObject()
+                let propertyId = item.propertyId._id;
                 let imageData = await Models.PImageDB.findOne({ _id: propertyId });
                 let mainImage = imageData ? imageData.mainImage : [];
-                Data[x].images = mainImage;
+                item.images = mainImage;
+                result.push(item)
             }
-            console.log('Data is', Data)
-            if (Data) {
+            console.log('Data is', result)
+            if (result) {
                 // if data found check verified or not
-                res.send({ status: true, message: "User Booking List", data: Data });
+                res.send({ status: true, message: "User Booking List", data: result });
             } else {
                 res.send({ status: true, message: "User Booking not found" });
             }
