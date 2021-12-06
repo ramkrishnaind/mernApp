9eea42b323c83d83c549dc2967d2f2b6d85922b7
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2021-12-06 15:40:49 +0530

    Online booking changes related to layout

--- client/src/pages/online-form/banner/banner-img/banner-img.js
+++ client/src/pages/online-form/banner/banner-img/banner-img.js
@@ -1,16 +1,18 @@
-import { Box, Container, Image } from '@material-ui/core';
-import React from 'react';
-import BannerContent from '../banner-content/banner-content';
-import "./banner-img.css"
+import { Box, Container, Image } from "@material-ui/core";
+import React from "react";
+import BannerContent from "../banner-content/banner-content";
+import "./banner-img.css";
 
-
-function BannerImage(){
-    return(
-        <Box className="onlineFormHeader">
-            <img src={process.env.PUBLIC_URL + '/banner/bookonline.jpg'} />
-            <BannerContent />
-        </Box>
-    )
+function BannerImage() {
+  return (
+    <Box
+      className="onlineFormHeader"
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <img src={process.env.PUBLIC_URL + "/banner/bookonline.jpg"} />
+      <BannerContent />
+    </Box>
+  );
 }
 
 export default BannerImage;
