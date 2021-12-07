d80d95b97ba5a28efa5e2a245511f98ea7331d8b
vikramsinwal <vikramsinwal@gmail.com>
2021-12-07 13:27:19 +0530

    Dashboard Bug fix

--- Server/Controllers/Routes/HomeModule/dashBoardModule.js
+++ Server/Controllers/Routes/HomeModule/dashBoardModule.js
@@ -9,50 +9,56 @@ const CONSTANTSMESSAGE = require('../../../Helper/constantsMessage')
 function getDashBoardData(Models) {
     async function getFun(req, res) {
         try {
+            let totalUsersCount = await totalUsers();
+            let activeUsersCount = await activeUsers();
+            let inActiveCount = totalUsersCount - activeUsersCount;
+
             let results = {
                 users: {
-                    total: 15,
-                    active: 12,
-                    inActive: 3
+                    total: totalUsersCount,
+                    active: activeUsersCount,
+                    inActive: inActiveCount
                 },
                 properties: {
                     sell: {
-                        residential: 15,
-                        commercial: 10
+                        residential: await sellResidential(),
+                        commercial: await sellCommercial()
                     },
                     rent: {
-                        residential: 20,
-                        commercial: 5
+                        residential: await rentResidential(),
+                        commercial: await rentCommercial()
                     }
                 },
-                booking: 100,
+                booking: await totalBookings(),
                 enquiry: {
-                    open: 51,
-                    close: 12
+                    open: await openEnquiry(),
+                    close: await closeEnquiry()
                 },
                 siteVisit: {
-                    open: 41,
-                    close: 56
+                    open: await openSiteVisit(),
+                    close: await closeSiteVisit()
                 },
                 callBack: {
-                    open: 56,
-                    close: 25
+                    open: await openCallBack(),
+                    close: await closeCallBack()
                 },
                 contactWithUs: {
-                    open: 45,
-                    close: 56
+                    open: await openContactWithUs(),
+                    close: await closeContactWithUs()
                 },
                 jobApplications: {
-                    open: 44,
-                    close: 21
+                    all: await allJobApplications(),
+                    open: await openJobApplications(),
+                    close: await closeJobApplications(),
+                    rejected: await rejectedJobApplications()
                 },
                 suppliers: {
-                    open: 20,
-                    close: 30
+                    open: await openSuppliers(),
+                    close: await closeSuppliers()
                 },
                 servicesEnquiry: {
-                    open: 10,
-                    close: 20
+                    open: await openServicesEnquiry(),
+                    close: await closeServicesEnquiry()
                 }
             }
             res.send({ status: true, message: "Dashboard Details", data: results });
@@ -65,5 +71,76 @@ function getDashBoardData(Models) {
         }
     }
     return getFun;
+    async function totalUsers() {
+        return totalUsers = await Models.UserDB.countDocuments({});
+    }
+    async function activeUsers() {
+        return activeUsers = await Models.UserDB.countDocuments({ active: 1 });
+    }
+    async function sellResidential() {
+        return sellResidential = await Models.PropertyDB.countDocuments({ for: 'Sell', pType: 'RESIDENTIAL' });
+    }
+    async function sellCommercial() {
+        return sellCommercial = await Models.PropertyDB.countDocuments({ for: 'Sell', pType: 'COMMERCIAL' });
+    }
+    async function rentResidential() {
+        return rentResidential = await Models.PropertyDB.countDocuments({ for: 'Rent', pType: 'RESIDENTIAL' });
+    }
+    async function rentCommercial() {
+        return rentCommercial = await Models.PropertyDB.countDocuments({ for: 'Rent', pType: 'COMMERCIAL' });
+    }
+    async function totalBookings() {
+        return bookings = await Models.BookingDB.countDocuments({});
+    }
+    async function openEnquiry() {
+        return openEnquiry = await Models.EnquiryDB.countDocuments({ status: true });
+    }
+    async function closeEnquiry() {
+        return closeEnquiry = await Models.EnquiryDB.countDocuments({ status: false });
+    }
+    async function openSiteVisit() {
+        return openSiteVisit = await Models.SiteVisitDB.countDocuments({ status: true });
+    }
+    async function closeSiteVisit() {
+        return closeSiteVisit = await Models.SiteVisitDB.countDocuments({ status: false });
+    }
+    async function openCallBack() {
+        return openCallBack = await Models.ReqCallbackDB.countDocuments({ status: true });
+    }
+    async function closeCallBack() {
+        return closeCallBack = await Models.ReqCallbackDB.countDocuments({ status: false });
+    }
+    async function openContactWithUs() {
+        return openContactWithUs = await Models.ContactUsDB.countDocuments({ isResolved: false });
+    }
+    async function closeContactWithUs() {
+        return closeContactWithUs = await Models.ContactUsDB.countDocuments({ isResolved: true });
+    }
+    async function allJobApplications() {
+        return allJobApplications = await Models.JobApplicationDB.countDocuments({});
+    }
+    async function openJobApplications() {
+        return openJobApplications = await Models.JobApplicationDB.countDocuments({ status: 0 });
+    }
+    async function closeJobApplications() {
+        return closeJobApplications = await Models.JobApplicationDB.countDocuments({ status: 1 });
+    }
+    async function rejectedJobApplications() {
+        return rejectedJobApplications = await Models.JobApplicationDB.countDocuments({ status: 2 });
+    }
+    async function openSuppliers() {
+        return openSuppliers = await Models.SupplierDB.countDocuments({ isActive: true });
+    }
+    async function closeSuppliers() {
+        return closeSuppliers = await Models.SupplierDB.countDocuments({ isActive: false });
+    }
+    async function openServicesEnquiry() {
+        return openServicesEnquiry = await Models.ServiceEnquiryDB.countDocuments({ active: true });
+    }
+    async function closeServicesEnquiry() {
+        return closeServicesEnquiry = await Models.ServiceEnquiryDB.countDocuments({ active: false });
+    }
+
+
 }
 module.exports = getDashBoardData;
