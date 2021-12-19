cd3ca2b793622997d30287b672e883d832daf2ba
vikramsinwal <vikramsinwal@gmail.com>
2021-12-19 21:39:40 +0530

    Sms and Emails

--- Server/Controllers/Routes/BookingModule/createBookingModule.js
+++ Server/Controllers/Routes/BookingModule/createBookingModule.js
@@ -1,9 +1,13 @@
 const _ = require('lodash');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi)
+const path = require("path");
 
 const errorResponseHelper = require('../../../Helper/errorResponse');
 const CONSTANTSMESSAGE = require('../../../Helper/constantsMessage')
+const sendSupplierMailHelper = require('../../../Helper/sendSupplierMailHelper');
+const SendMessage = require('../../../Helper/sendSms');
+
 const moduleSchema = Joi.object({
     propertyId: Joi.objectId().trim().required(),
     name: Joi.string().required(),
@@ -30,34 +34,42 @@ const siteMobile = async (Models) => {
 };
 const sendMessageToClient = async (Models, bodyData) => {
     let clientMessage = 'Hello ' + bodyData.name + ', \n';
-    let clientMobile = bodyData.phone;
+    let clientMobile = bodyData.mobile;
     let mobile = await siteMobile(Models);
-    clientMessage += 'Your Request Received For Site Visit. We will get back to you at the earliest For any query Call ' + mobile + ' \n';
-    clientMessage += 'Regards, Vishal Construction Company VISHCC';
+    clientMessage += 'Your Booking Request Received. We will get back to you at the earliest For any query Call ' + mobile + ' \n';
+    clientMessage += 'Regards, \n';
+    clientMessage += 'Vishal Construction Company \n';
+    clientMessage += 'VCCFLT';
+
     console.log('clientMessage is', clientMessage);
     await SendMessage({
-        senderID: 'VISHCC',
-        templateID: '1207163549228584692',
+        senderID: 'VCCFLT',
+        templateID: '1207163679254175639',
         mobile: clientMobile,
         message: clientMessage,
     });
 };
+
 const sendMessageToAdmin = async (Models, bodyData) => {
     let mobile = await siteMobile(Models);
+    let PropertyData = await getPropertyDetails(Models, bodyData.propertyId);
+    console.log('PropertyData in sendMessage is', PropertyData)
     let message = 'Hello Vishal Properties, \n';
-    message += 'A New Request Received For Site Visit. \n';
-    message += 'Name : ' + bodyData.name + ' \n';
-    message += 'Mobile : ' + bodyData.phone + ' \n';
-    message += 'Email : ' + bodyData.email + ' \n';
-    message += 'Time : ' + bodyData.time + ' \n';
-    message += 'Thanks \n';
-    message += 'VISHCC';
+    message += bodyData.name + ' Booked a FlatNo. ' + bodyData.flatNumber + ' of  ' + PropertyData ? PropertyData.pType : '' + '  property named as  ' + PropertyData ? PropertyData.nameOfProject : '' + '  with a Amount of Rs.  ' + bodyData.bookingAmount + '. \n';
+    message += ' Please call back (Mobile:-  ' + bodyData.mobile + ' )  \n';
+    message += 'Email:  ' + bodyData.email + ' , \n';
+    message += 'Pan Number:  ' + bodyData.panCard + ', \n';
+    message += 'City:  ' + bodyData.city + ', \n';
+    message += 'State:  ' + bodyData.state + ' , \n';
+    message += 'PinNumber:  ' + bodyData.pinCode + ', \n';
+    message += 'Country:  ' + bodyData.country + ' \n';
+    message += 'Thanks VISHCC';
     //let message = 'Hello, Vishal Propertie&nbsp; A New Request For Site Visit&nbsp;Name : ' + bodyData.name + '&nbsp;Mobile : ' + bodyData.phone + '&nbsp;Email : ' + bodyData.email + '&nbsp;Time ' + bodyData.time + 'Message By:- Dzone india.&nbsp;Thanks';
     console.log('message is', message);
     await SendMessage({
         senderID: 'VISHCC',
-        templateID: '1207163549207674746',
-        mobile,
+        templateID: '1207163912526193433',
+        mobile: 9802953333,
         message
     });
 };
@@ -78,10 +90,11 @@ function createBookingRequest(Models) {
 
             let saveModule = await new Models.BookingDB(bodyData).save();
             console.log('saveModule is', saveModule)
-            // if (saveModule) {
-            //     await sendMessageToAdmin(Models, bodyData);
-            //     await sendMessageToClient(Models, bodyData);
-            // }
+            if (saveModule) {
+                await sendMessageToAdmin(Models, bodyData);
+                await sendMessageToClient(Models, bodyData);
+                await sendBookingMail(Models, bodyData)
+            }
             res.send({ status: true, message: CONSTANTSMESSAGE.CREATE_SUCCESS_MESSAGE });
         }
         catch (e) {
@@ -90,5 +103,42 @@ function createBookingRequest(Models) {
         }
     }
     return create;
+}
+async function getPropertyDetails(Models, propertyId) {
+    try {
+        let result = await Models.PropertyDB.findOne({ _id: propertyId }).lean();
+        return result;
+    } catch (error) {
+        return {};
+    }
+}
+async function sendBookingMail(Models, data) {
+    try {
+        let filePath = path.join(__dirname, '/../../../Template/booknow.html');
+        let PropertyData = await getPropertyDetails(Models, data.propertyId);
+        let replacements = {
+            name: `${_.capitalize(data.name)}`,
+            project: PropertyData ? PropertyData.nameOfProject : '',
+            propertyCode: PropertyData ? PropertyData.propertyCode : '',
+            mobile: data.mobile,
+            email: data.email,
+            panCard: data.panCard,
+            propertyType: PropertyData ? PropertyData.pType : '',
+            country: data.country,
+            state: data.state,
+            city: data.city,
+            pinCode: data.pinCode,
+            bookingAmount: data.bookingAmount,
+            flatNumber: data.flatNumber
+        }
+
+        //let info = await prepareTemplateAndMailHelper({ filePath, replacements, to: data.email, subject: "New Supplier Request For VCC" });
+        let attachments = '';
+        let info = await sendSupplierMailHelper({ filePath, replacements, to: "info@vishalconstructioncompany.com", subject: "New Booking from VCC", attachments, from: data.email });
+        return info;
+    }
+    catch (e) {
+        console.log('error', e);
+    }
 }
 module.exports = createBookingRequest;
--- Server/Controllers/Routes/ServicesModule/createServicesEnquiryModule.js
+++ Server/Controllers/Routes/ServicesModule/createServicesEnquiryModule.js
@@ -39,6 +39,11 @@ function createServicesEnquiry(Models) {
             bodyData.image = req.files;
             let saveModule = await new Models.ServiceEnquiryDB(bodyData).save();
             console.log('saveModule is', saveModule)
+            if (saveModule) {
+                await sendMessageToAdmin(Models, bodyData);
+                await sendMessageToClient(Models, bodyData);
+                await sendMail(Models, bodyData)
+            }
             res.send({ status: true, message: CONSTANTSMESSAGE.CREATE_SUCCESS_MESSAGE });
         }
         catch (e) {
@@ -48,4 +53,94 @@ function createServicesEnquiry(Models) {
     }
     return create;
 }
+async function sendMail(Models, data) {
+    try {
+        let filePath = path.join(__dirname, '/../../../Template/booknow.html');
+        let replacements = {
+            name: `${_.capitalize(data.name)}`,
+            mobile: data.mobile,
+            email: data.email,
+            address: data.address,
+            propertyType: data.propertyType,
+            state: data.state,
+            city: data.city,
+            propertyAddress: data.propertyAddress,
+            propertyCity: data.propertyCity,
+            propertyState: data.propertyState,
+            propertyLocation: data.propertyLocation,
+            budget: data.budget,
+            totalArea: data.totalArea,
+            floor: data.floor,
+            message: data.message
+        }
+
+        //let info = await prepareTemplateAndMailHelper({ filePath, replacements, to: data.email, subject: "New Supplier Request For VCC" });
+        let attachments = '';
+        let info = await sendSupplierMailHelper({ filePath, replacements, to: "info@vishalconstructioncompany.com", subject: "New Booking from VCC", attachments, from: data.email });
+        return info;
+    }
+    catch (e) {
+        console.log('error', e);
+    }
+}
+const siteMobile = async (Models) => {
+    let result = await Models.AddressDB.findOne({ active: true })
+    console.log('result is', result)
+    if (result) {
+        return result.mobile;
+    } else {
+        return 9802953333;
+    }
+};
+const sendMessageToClient = async (Models, bodyData) => {
+    let clientMessage = 'Hello ' + bodyData.name + ', \n';
+    let clientMobile = bodyData.mobile;
+    let mobile = await siteMobile(Models);
+    clientMessage += 'Your Service Request Received. We will get back to you at the earliest For any query Call ' + mobile + '\n';
+    clientMessage += 'Regards, \n';
+    clientMessage += 'Vishal Construction Company \n';
+    clientMessage += 'VCCFLT';
+
+    console.log('clientMessage is', clientMessage);
+    await SendMessage({
+        senderID: 'VCCFLT',
+        templateID: '1207163679313251183',
+        mobile: clientMobile,
+        message: clientMessage,
+    });
+};
+
+const sendMessageToAdmin = async (Models, bodyData) => {
+    let mobile = await siteMobile(Models);
+    let PropertyData = await getPropertyDetails(Models, bodyData.propertyId);
+    console.log('PropertyData in sendMessage is', PropertyData)
+    let message = 'Hello Vishal Properties, \n';
+    message += bodyData.name + 'Requested a Service Enquiry. \n';
+    //message += ' Please call back (Mobile:-  ' + bodyData.mobile + ' )  \n';
+    message += 'Email:  ' + bodyData.email + ' \n';
+    message += 'Property Type:  ' + bodyData.propertyType + ' \n';
+    message += 'Property Address: ' + bodyData.city + ' \n';
+    message += 'Customer Address: ' + bodyData.state + '  \n';
+    message += 'Customer City: ' + bodyData.pinCode + ' \n';
+    message += 'Customer State: ' + bodyData.country + ' \n';
+    message += 'Property Details:- \n';
+    message += 'Property City: ' + bodyData.city + '\n';
+    message += 'Property State: ' + bodyData.state + ' \n';
+    message += 'Property Location : ' + bodyData.pinCode + ' \n';
+    message += 'Budget: ' + bodyData.country + ' \n';
+    message += 'Total Area: ' + bodyData.city + ' \n';
+    message += 'Floor: ' + bodyData.state + '  \n';
+    message += 'Message: ' + bodyData.pinCode + ' \n';
+    message += 'Regards, \n';
+    message += 'Vishal Construction Company';
+    message += 'VCCFLT';
+    //let message = 'Hello, Vishal Propertie&nbsp; A New Request For Site Visit&nbsp;Name : ' + bodyData.name + '&nbsp;Mobile : ' + bodyData.phone + '&nbsp;Email : ' + bodyData.email + '&nbsp;Time ' + bodyData.time + 'Message By:- Dzone india.&nbsp;Thanks';
+    console.log('message is', message);
+    await SendMessage({
+        senderID: 'VCCFLT',
+        templateID: '1207163679298339487',
+        mobile: 9802953333,
+        message
+    });
+};
 module.exports = createServicesEnquiry;
