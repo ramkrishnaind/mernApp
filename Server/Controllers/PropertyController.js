c5fe076eac435158742ff817ed0038a2fc95ee2f
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-05-12 18:33:00 +0530

    added route to delete images

--- Server/Controllers/PropertyController.js
+++ Server/Controllers/PropertyController.js
@@ -1,64 +1,151 @@
-
-const express = require('express');
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 var router = express.Router();
 const multer = require("multer");
-const path = require('path');
+const path = require("path");
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log(' image data is', req.body)
-        let fpathId = 'uploads/property/' + req.body.propertyId;
-        try {
-            if (!fs.existsSync(fpathId)) {
-                fs.mkdirSync(fpathId, { recursive: true }, (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                });
-            }
-        } catch (err) {
-            console.error('axios Error ', err)
-        }
-        cb(null, fpathId)
-    },
-    filename: (req, file, cb) => {
-        req.body.imageFileName = (file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  destination: (req, file, cb) => {
+    console.log(" image data is", req.body);
+    let fpathId = "uploads/property/" + req.body.propertyId;
+    try {
+      if (!fs.existsSync(fpathId)) {
+        fs.mkdirSync(fpathId, { recursive: true }, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    } catch (err) {
+      console.error("axios Error ", err);
     }
+    cb(null, fpathId);
+  },
+  filename: (req, file, cb) => {
+    req.body.imageFileName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 let upload = multer({ storage: storage });
 
-const propertyCommonHelper = require('./Routes/PropertyModule/propertyCommonHelper');
-let { createPropertyRequest, getPropertyRequest, updatePropertyRequest, updatePropertyStatusRequest, exteriorImage,
-    getUserIdPropertyList, updatePrice, getSearchPropertyList } = require('./Routes');
-const userAuthMiddlewareFunction = require('../Middleware/userAuth');
+const propertyCommonHelper = require("./Routes/PropertyModule/propertyCommonHelper");
+let {
+  createPropertyRequest,
+  getPropertyRequest,
+  updatePropertyRequest,
+  updatePropertyStatusRequest,
+  exteriorImage,
+  getUserIdPropertyList,
+  updatePrice,
+  getSearchPropertyList,
+  deletePropertyImageRequest,
+} = require("./Routes");
+const userAuthMiddlewareFunction = require("../Middleware/userAuth");
 
 module.exports = function (conn) {
-    // console.log(conn)
-    const allCollection = require('../Database/getCollections')(conn.MongoDBConnection);
-    const userAuthMiddleware = userAuthMiddlewareFunction.userAuthMiddleware(allCollection);
-    const requestAuthMiddleware = userAuthMiddlewareFunction.requestAuthMiddleware(allCollection);
+  // console.log(conn)
+  const allCollection = require("../Database/getCollections")(
+    conn.MongoDBConnection
+  );
+  const userAuthMiddleware =
+    userAuthMiddlewareFunction.userAuthMiddleware(allCollection);
+  const requestAuthMiddleware =
+    userAuthMiddlewareFunction.requestAuthMiddleware(allCollection);
 
-    router.post('/createPropertyRequest', userAuthMiddleware, createPropertyRequest(allCollection))
-    router.post('/createProperty', requestAuthMiddleware, createPropertyRequest(allCollection))
-    router.post('/updateProperty', userAuthMiddleware, updatePropertyRequest(allCollection))
-    router.post('/deleteProperty', userAuthMiddleware, propertyCommonHelper.deleteProperty(allCollection))
-    router.post('/propertyDetail', requestAuthMiddleware, propertyCommonHelper.propertyDetail(allCollection))
-    router.post('/getPropertyDetail', userAuthMiddleware, propertyCommonHelper.propertyDetail(allCollection))
-    router.post('/getAllProperty', userAuthMiddleware, propertyCommonHelper.getAllPropertyForAdmin(allCollection))
-    router.post('/getAllPropertyForClient', requestAuthMiddleware, propertyCommonHelper.getAllProperty(allCollection))
-    router.post('/getAllPropertyForHome', requestAuthMiddleware, propertyCommonHelper.getHomeAllProperty(allCollection))
-    router.post('/getPropertyRequest', requestAuthMiddleware, getPropertyRequest(allCollection))
-    router.post('/updatePropertyStatusRequest', userAuthMiddleware, updatePropertyStatusRequest(allCollection))
-    router.post('/uploadImage', upload.array("image"), exteriorImage(allCollection))
-    router.post('/getUserIdPropertyRequest', getUserIdPropertyList(allCollection))
-    router.post('/updatePrice', updatePrice(allCollection))
-    router.post('/getSearchPropertyList', requestAuthMiddleware, getSearchPropertyList(allCollection))
-    router.post('/getSearchTerms', requestAuthMiddleware, propertyCommonHelper.getSearchTerms(allCollection))
-    router.post('/getPropertyLatLong', requestAuthMiddleware, propertyCommonHelper.getPropertyLatLong(allCollection))
-    router.post('/getsearchMinMax', requestAuthMiddleware, propertyCommonHelper.getsearchMinMax(allCollection))
-    router.post('/getPropertyByType', requestAuthMiddleware, propertyCommonHelper.getPropertyByType(allCollection))
+  pau9Cz2QUSxy;
+  router.post(
+    "/createProperty",
+    requestAuthMiddleware,
+    createPropertyRequest(allCollection)
+  );
+  router.post(
+    "/updateProperty",
+    userAuthMiddleware,
+    updatePropertyRequest(allCollection)
+  );
+  router.post(
+    "/deleteProperty",
+    userAuthMiddleware,
+    propertyCommonHelper.deleteProperty(allCollection)
+  );
+  router.post(
+    "/deletePropertyImage",
+    userAuthMiddleware,
+    deletePropertyImageRequest(allCollection)
+  );
+  router.post(
+    "/propertyDetail",
+    requestAuthMiddleware,
+    propertyCommonHelper.propertyDetail(allCollection)
+  );
+  router.post(
+    "/getPropertyDetail",
+    userAuthMiddleware,
+    propertyCommonHelper.propertyDetail(allCollection)
+  );
+  router.post(
+    "/getAllProperty",
+    userAuthMiddleware,
+    propertyCommonHelper.getAllPropertyForAdmin(allCollection)
+  );
+  router.post(
+    "/getAllPropertyForClient",
+    requestAuthMiddleware,
+    propertyCommonHelper.getAllProperty(allCollection)
+  );
+  router.post(
+    "/getAllPropertyForHome",
+    requestAuthMiddleware,
+    propertyCommonHelper.getHomeAllProperty(allCollection)
+  );
+  router.post(
+    "/getPropertyRequest",
+    requestAuthMiddleware,
+    getPropertyRequest(allCollection)
+  );
+  router.post(
+    "/updatePropertyStatusRequest",
+    userAuthMiddleware,
+    updatePropertyStatusRequest(allCollection)
+  );
+  router.post(
+    "/uploadImage",
+    upload.array("image"),
+    exteriorImage(allCollection)
+  );
+  router.post(
+    "/getUserIdPropertyRequest",
+    getUserIdPropertyList(allCollection)
+  );
+  router.post("/updatePrice", updatePrice(allCollection));
+  router.post(
+    "/getSearchPropertyList",
+    requestAuthMiddleware,
+    getSearchPropertyList(allCollection)
+  );
+  router.post(
+    "/getSearchTerms",
+    requestAuthMiddleware,
+    propertyCommonHelper.getSearchTerms(allCollection)
+  );
+  router.post(
+    "/getPropertyLatLong",
+    requestAuthMiddleware,
+    propertyCommonHelper.getPropertyLatLong(allCollection)
+  );
+  router.post(
+    "/getsearchMinMax",
+    requestAuthMiddleware,
+    propertyCommonHelper.getsearchMinMax(allCollection)
+  );
+  router.post(
+    "/getPropertyByType",
+    requestAuthMiddleware,
+    propertyCommonHelper.getPropertyByType(allCollection)
+  );
 
-
-    return router;
+  return router;
 };
---
+++ Server/Controllers/Routes/PropertyModule/deletePropertyImage.js
@@ -0,0 +1,74 @@
+const _ = require("lodash");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const errorResponseHelper = require("../../../Helper/errorResponse");
+const CONSTANTSMESSAGE = require("../../../Helper/constantsMessage");
+const moduleSchema = Joi.object({
+  propertyId: Joi.string().empty("").required(),
+  imageKey: Joi.string().empty("").required(),
+  fileName: Joi.string().empty("").required(),
+});
+
+function deletePropertyImageRequest(Models) {
+  async function deleteImage(req, res) {
+    try {
+      // validate data using joi
+      let validateData = moduleSchema.validate(req.body);
+      if (validateData.error) {
+        throw {
+          status: false,
+          error: validateData,
+          message: CONSTANTSMESSAGE.INVALID_DATA,
+        };
+      }
+
+      // pick data from req.body
+      let bodyDataPropImage = _.pick(req.body, [
+        "propertyId",
+        "imageKey",
+        "fileName",
+      ]);
+
+      if (
+        bodyDataPropImage.imageKey !== "mainImage" &&
+        bodyDataPropImage.imageKey !== "exteriorView" &&
+        bodyDataPropImage.imageKey !== "livingRoom" &&
+        bodyDataPropImage.imageKey !== "badrooms" &&
+        bodyDataPropImage.imageKey !== "bathrooms" &&
+        bodyDataPropImage.imageKey !== "kitchen" &&
+        bodyDataPropImage.imageKey !== "floorPlan" &&
+        bodyDataPropImage.imageKey !== "masterPlan" &&
+        bodyDataPropImage.imageKey !== "other"
+      ) {
+        throw {
+          status: false,
+          error: "invalid Image key",
+          message: "invalid Image key",
+        };
+      }
+      const pImageData = await Models.PImageDB.findById(propertyId);
+      pImageData[bodyDataPropImage.imageKey] = pImageData[
+        bodyDataPropImage.imageKey
+      ].filter((imageObj) => imageObj.filename !== bodyDataPropImage.fileName);
+
+      await Models.PImageDB.findByIdAndUpdate(propertyId, {
+        [bodyDataPropImage.imageKey]: pImageData[bodyDataPropImage.imageKey],
+      });
+      res.send({
+        status: true,
+        propertyId: saveModule._id,
+        message: CONSTANTSMESSAGE.CREATE_SUCCESS_MESSAGE,
+      });
+    } catch (e) {
+      console.log("saveModule err", e);
+      await errorResponseHelper({
+        res,
+        error: e,
+        defaultMessage: "Error in saveModule",
+      });
+    }
+  }
+  return deleteImage;
+}
+module.exports = deletePropertyImageRequest;
