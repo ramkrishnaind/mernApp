3828857237f62203b53c5642e377ae1a46312dd3
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-07-20 21:24:47 +0530

    TLS error

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,22 @@
-
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const helmet = require("helmet");
-const http = require('http');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-
+const http = require("http");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var app = express();
 var PORT = process.env.PORT || 3333;
-var config = require('./config.json');
+var config = require("./config.json");
 
 app.disable("x-powered-by");
-app.use(helmet({
-    contentSecurityPolicy: false
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // white listing origin from infinityCoding.com
 // let whitelist = [/\.infinitycoding\.in$/, "http://192.46.214.45:3333", "http://192.46.214.45:5555", "http://192.46.214.45:9999"];
@@ -27,55 +28,137 @@ app.use(helmet({
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     credentials: true
 // }));
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-    secret: 'VikramJeetSinwal',
+app.use(
+  session({
+    secret: "VikramJeetSinwal",
     resave: false,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 //const dir = path.join(__dirname, '/uploads');
 //app.use("/uploads", express.static(dir));
-app.use("/uploads", express.static('uploads'));
-app.set('trust proxy', 1);
+app.use("/uploads", express.static("uploads"));
+app.set("trust proxy", 1);
 
-
-const MongoDBConnection = require('./Database/connection');
+const MongoDBConnection = require("./Database/connection");
 //const MySqlConnection = require('./Database/gammaDBConnection/connection');
 
-var prefix = '/api/';
+var prefix = "/api/";
 
 //app.use('/api/pricing', require('./controllers/pricing.controller')({ MongoDBConnection }));
 console.log();
-app.use(`${prefix}users`, require('./Controllers/UsersController')({ MongoDBConnection }));
-app.use(`${prefix}menuModule`, require('./Controllers/MenuModuleController')({ MongoDBConnection }));
-app.use(`${prefix}role`, require('./Controllers/RoleController')({ MongoDBConnection }));
-app.use(`${prefix}callback`, require('./Controllers/CallbackController')({ MongoDBConnection }));
-app.use(`${prefix}enquiry`, require('./Controllers/EnquiryController')({ MongoDBConnection }));
-app.use(`${prefix}review`, require('./Controllers/ReviewController')({ MongoDBConnection }));
-app.use(`${prefix}property`, require('./Controllers/PropertyController')({ MongoDBConnection }));
-app.use(`${prefix}feedback`, require('./Controllers/ClientFeedbackController')({ MongoDBConnection }));
-app.use(`${prefix}cms`, require('./Controllers/CMSController')({ MongoDBConnection }));
-app.use(`${prefix}slider`, require('./Controllers/SliderController')({ MongoDBConnection }));
-app.use(`${prefix}builder`, require('./Controllers/BuildingMaterialsController')({ MongoDBConnection }));
-app.use(`${prefix}services`, require('./Controllers/ServicesController')({ MongoDBConnection }));
-app.use(`${prefix}contactus`, require('./Controllers/ContactUsController')({ MongoDBConnection }));
-app.use(`${prefix}sitevisit`, require('./Controllers/siteVisitController')({ MongoDBConnection }));
-app.use(`${prefix}career`, require('./Controllers/CareerController')({ MongoDBConnection }));
-app.use(`${prefix}blog`, require('./Controllers/BlogController')({ MongoDBConnection }));
-app.use(`${prefix}home`, require('./Controllers/HomeController')({ MongoDBConnection }));
-app.use(`${prefix}team`, require('./Controllers/OurTeamController')({ MongoDBConnection }));
-app.use(`${prefix}booking`, require('./Controllers/BookingController')({ MongoDBConnection }));
-app.use(`${prefix}constructionProcess`, require('./Controllers/ConstructionProcessController')({ MongoDBConnection }));
-app.use(`${prefix}investWithUs`, require('./Controllers/InvestWithUsController')({ MongoDBConnection }));
-app.use(`${prefix}newsLetter`, require('./Controllers/NewsLetterController')({ MongoDBConnection }));
-app.use(`${prefix}aboutPage`, require('./Controllers/AboutController')({ MongoDBConnection }));
-app.use(`${prefix}finance`, require('./Controllers/FinanceController')({ MongoDBConnection }));
-app.use(`${prefix}otp`, require('./Controllers/OtpController')({ MongoDBConnection }));
-app.use(`${prefix}supplier`, require('./Controllers/SupplierController')({ MongoDBConnection }));
-app.use(`${prefix}deleteImage`, require('./Controllers/DeleteImageController')({ MongoDBConnection }));
+app.use(
+  `${prefix}users`,
+  require("./Controllers/UsersController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}menuModule`,
+  require("./Controllers/MenuModuleController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}role`,
+  require("./Controllers/RoleController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}callback`,
+  require("./Controllers/CallbackController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}enquiry`,
+  require("./Controllers/EnquiryController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}review`,
+  require("./Controllers/ReviewController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}property`,
+  require("./Controllers/PropertyController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}feedback`,
+  require("./Controllers/ClientFeedbackController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}cms`,
+  require("./Controllers/CMSController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}slider`,
+  require("./Controllers/SliderController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}builder`,
+  require("./Controllers/BuildingMaterialsController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}services`,
+  require("./Controllers/ServicesController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}contactus`,
+  require("./Controllers/ContactUsController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}sitevisit`,
+  require("./Controllers/siteVisitController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}career`,
+  require("./Controllers/CareerController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}blog`,
+  require("./Controllers/BlogController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}home`,
+  require("./Controllers/HomeController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}team`,
+  require("./Controllers/OurTeamController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}booking`,
+  require("./Controllers/BookingController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}constructionProcess`,
+  require("./Controllers/ConstructionProcessController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}investWithUs`,
+  require("./Controllers/InvestWithUsController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}newsLetter`,
+  require("./Controllers/NewsLetterController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}aboutPage`,
+  require("./Controllers/AboutController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}finance`,
+  require("./Controllers/FinanceController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}otp`,
+  require("./Controllers/OtpController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}supplier`,
+  require("./Controllers/SupplierController")({ MongoDBConnection })
+);
+app.use(
+  `${prefix}deleteImage`,
+  require("./Controllers/DeleteImageController")({ MongoDBConnection })
+);
 
 // let reactHTMLPath = process.env.NODE_ENV === 'production' ? "../FX-React/build" : "";
 
@@ -90,6 +173,5 @@ app.use(`${prefix}deleteImage`, require('./Controllers/DeleteImageController')({
 
 var server = http.createServer(app);
 server.listen(PORT, () => {
-    console.log("Server started at", PORT);
+  console.log("Server started at", PORT);
 });
-
